100698dda7b68d4e52c48678c3d24bad4f84787b
Chinatu Lucia Eke <chinatuluciaeke@gmail.com>
2024-07-14 20:20:34 +0100

    feat: set up routes and pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,64 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+    Layout,
+    Home,
+    About,
+    Products,
+    Product,
+    Contact,
+    Cart,
+    Checkout,
+    NotFound,
+    Error,
+} from "./pages";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Layout />,
+        errorElement: <NotFound />,
+        children: [
+            {
+                index: true,
+                element: <Home />,
+                errorElement: <Error />,
+            },
+            {
+                path: "about",
+                element: <About />,
+                errorElement: <Error />,
+            },
+            {
+                path: "products",
+                element: <Products />,
+                errorElement: <Error />,
+            },
+            {
+                path: "products/:id",
+                element: <Product />,
+                errorElement: <Error />,
+            },
+            {
+                path: "contact",
+                element: <Contact />,
+                errorElement: <Error />,
+            },
+            {
+                path: "cart",
+                element: <Cart />,
+                errorElement: <Error />,
+            },
+            {
+                path: "checkout",
+                element: <Checkout />,
+                errorElement: <Error />,
+            },
+        ],
+    },
+]);
+
 function App() {
-  return (
-    <h1 className="text-3xl">sneaks</h1>
-  )
+    return <RouterProvider router={router}></RouterProvider>;
 }
 
-export default App
+export default App;

7ce4a474784b1cce8a16258a8e63a0866d603ef5
Chinatu Lucia Eke <chinatuluciaeke@gmail.com>
2024-08-09 20:02:00 +0100

    feat:add modal to confirm clear cart functionality

--- src/components/CartItemsList.tsx
+++ src/components/CartItemsList.tsx
@@ -1,5 +1,7 @@
+import { useState } from "react";
 import { useCartContext } from "@context/cartContext";
 import type { CartItem as CartItemType } from "@lib/definitions";
+import Modal from "./Modal";
 import CartItem from "./CartItem";
 
 type CartItemsListProps = {
@@ -7,7 +9,16 @@ type CartItemsListProps = {
 };
 
 const CartItemsList = ({ cart }: CartItemsListProps) => {
+    const [modal, setModal] = useState(false);
     const { clearCart } = useCartContext();
+
+    const closeModal = () => {
+        setModal(false);
+    };
+
+    const openModal = () => {
+        setModal(true);
+    };
 
     return (
         <div className='grid gap-6 w-full lg:w-6/12 xl:w-7/12'>
@@ -18,11 +29,12 @@ const CartItemsList = ({ cart }: CartItemsListProps) => {
             </div>
             <div className='w-full h-[2px] bg-lightGray'></div>
             <button
-                onClick={clearCart}
+                onClick={openModal}
                 className='border border-black rounded py-2 px-4 text-sm font-medium transition-ease hover:bg-black hover:text-white lg:justify-self-end lg:w-max'
             >
                 Clear Cart
             </button>
+            {modal && <Modal closeModal={closeModal} clearCart={clearCart} />}
         </div>
     );
 };

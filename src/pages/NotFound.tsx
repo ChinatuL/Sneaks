afb0d72e2c96b6c84d599755f6e40623b2bdf791
Chinatu Lucia Eke <chinatuluciaeke@gmail.com>
2024-08-17 13:07:21 +0100

    feat: complete 404 page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,6 +1,15 @@
+import icon404 from "@icons/not-found.svg";
+
 const NotFound = () => {
-  return (
-    <div>NotFound</div>
-  )
-}
-export default NotFound
+    return (
+        <div className='flex justify-center items-center h-screen max-w-[1240px] mx-auto w-full px-4 lg:px-8 2xl:px-0'>
+            <div className='flex flex-col items-center'>
+                <img src={icon404} alt='Page not found' className='w-max' />
+                <p className='lg:text-2xl'>
+                    Sorry, the page you are looking for does not exist
+                </p>
+            </div>
+        </div>
+    );
+};
+export default NotFound;
